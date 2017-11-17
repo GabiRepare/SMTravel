f1402f28e5639be1528ac8cf69891bacbdb09f41
ginguan <yguan074@uottawa.ca>
2017-11-16 20:01:41 -0500

    Output Update

--- src/simModel/Output.java
+++ src/simModel/Output.java
@@ -13,6 +13,28 @@ class Output
     // DSOVs available in the OutputSequence objects
     // If seperate methods required to process Trajectory or Sample
     // Sequences - add them here
+	private int numLongWait;
+	private int numServed;
+	private int numBusySignal;
 
+	public int getNumLongWait() {
+		return numLongWait;
+	}
+	public void setNumLongWait(int numLongWait) {
+		this.numLongWait = numLongWait;
+	}
+	public int getNumServed() {
+		return numServed;
+	}
+	public void setNumServed(int numServed) {
+		this.numServed = numServed;
+	}
+	public int getNumBusySignal() {
+		return numBusySignal;
+	}
+	public void setNumBusySignal(int numBusySignal) {
+		this.numBusySignal = numBusySignal;
+	}
+	
     // SSOVs
 }
