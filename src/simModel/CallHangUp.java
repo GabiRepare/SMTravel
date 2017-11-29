21d10f64b12cc982fe29f445348925e8ae337e84
k8ch <kateryna.chernega@gmail.com>
2017-11-28 20:07:45 -0500

    renamed class to SMTravel and added CallHangUp

--- src/simModel/CallHangUp.java
+++ src/simModel/CallHangUp.java
@@ -1,5 +1,21 @@
 package simModel;
 
-class CallHangUp{
+
+class CallHangUp extends ScheduledAction{
+
+    simModel model; //reference to model object
+    CallHangUp(simModel caller){ this.model = model; }
+
+    public double timeSequence(){
+        //needs other classes
+    }
+
+    public void actionEvent(Call uCallType ){
+
+        //Call Hangup action sequence
+
+        //needs to be fixed based on other classes
+        model.qWaitLine.remove(uCallType);
+    }
 
 }
