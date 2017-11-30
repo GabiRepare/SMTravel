aa879ac53d9020a5c2e461c43d2c2435267eedfd
ginguan <yguan074@uottawa.ca>
2017-11-30 01:42:33 -0500

    update RVP

--- src/simModel/RVPs.java
+++ src/simModel/RVPs.java
@@ -34,16 +34,7 @@ class RVPs
 	    // clock value to get the next arrival time.
 	    return(nxtInterArr+model.getClock());
 	}
-	//we can choose 2 options here 
-	public double triangularDistribution(double a, double b, double c) {
-	    double F = (c - a) / (b - a);
-	    double rand = Math.random();
-	    if (rand < F) {
-	        return a + Math.sqrt(rand * (b - a) * (c - a));
-	    } else {
-	        return b - Math.sqrt((1 - rand) * (b - a) * (b - c));
-	    }
-	}
+
 	public double uServiceTime(Call.CallType callType, Operators.OperatorType operatorType){
 		if(callType.toString()=="INFORMATION" && operatorType.toString()=="REGULAR" ) {
 			return ThreadLocalRandom.current().nextDouble(1.2, 3.75);
