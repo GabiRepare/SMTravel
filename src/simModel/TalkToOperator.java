4a2dc1c9a7ad5e9d0be6c59bb50b70c8330aad91
Jp <jp.elrami@gmail.com>
2017-12-01 18:32:31 -0500

    changed Kate Talk to Operator code

--- src/simModel/TalkToOperator.java
+++ src/simModel/TalkToOperator.java
@@ -5,25 +5,38 @@ import simulationModelling.ConditionalActivity;
 class TalkToOperator extends ConditionalActivity {
     SMTravel model;
     //Customer Type is definied in Call.java, double check
-    private Call uCustomerType;
+    private Call call = new Call(model);
 
     public TalkToOperator(SMTravel model) { this.model = model; }
 
     protected static boolean precondition(SMTravel simModel){
-        boolean returnValue = false;
         //this may need to be modified based on other classes
-        if(simModel.rgOperator.type == simModel.n && simModel.n != 0){
-            returnValue = true;
+        if(call.uCostmerType == Constant.GOLD)
+        {
+            if(simModel.Operator[GOLD].numFreeOperator>0 ||
+               simModel.Operator[SILVER].numFreeOperator>0 ||
+               simModel.Operator[REGULAR].numFreeOperator>0)
+            {
+                return true;
+            }
         }
-        return(returnValue);
+        else if (call.uCostmerType == Constant.SILVER || Constant.REGULAR)
+        {
+            if(simModel.Operator[SILVER].numFreeOperator>0 ||
+               simModel.Operator[REGULAR].numFreeOperator>0
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void startingEvent(){
-        processTalkToOperator(customerType);
+        processTalkToOperator();
     }
 
     protected double duration(){
-        return (model.rvp.uServiceTime(uCustomerType));
+        return (simModel.rvp.uServiceTime(call.CallType, simModel.Operator.uOperatorType));
     }
 
     protected void secondaryEvent(){
@@ -32,7 +45,7 @@ class TalkToOperator extends ConditionalActivity {
     }
 
     protected double secondaryDuration(){
-        return (uAfterCallWorkTime(uCustomerType));
+        return (uAfterCallWorkTime(call.uCustomerType));
     }
 
     protected void terminatingEvent(){
